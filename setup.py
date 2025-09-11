52546641670f8d9c4d6cd81fa04d73de58aa6958
CyprienBouton <boutoncyprien@yahoo.com>
2025-09-11 14:19:22 +0200

    [up] ggrappa slicer

--- setup.py
+++ setup.py
@@ -21,6 +21,6 @@ setup(name='recomrzero', # this will be name of package in packages list : pip l
             'numpy',
             'torch',
             'nibabel',
-            'ggrappa @ git+https://github.com/CyprienBouton/ggrappa.git@fix_grid_size',
+            'ggrappa @ git+https://github.com/CyprienBouton/ggrappa.git@slicer',
       ]
      )
